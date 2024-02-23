42b4fdb4251652c9e75c39dbb393fa7cffdbb1a2
Ed <silva.email@yahoo.com>
2024-02-23 06:26:22 -0500

    Update Input and Login components

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Input = (props) => {
     return (
-        <div>
             <input type="text" placeholder={props.username}/>
-            <input type="password" placeholder={props.placeholder}/>
-        </div>
     );
 };
+
+
+export default Input;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Input from "./Input";
 
 const Login = () => {
     return       <form className="form">
