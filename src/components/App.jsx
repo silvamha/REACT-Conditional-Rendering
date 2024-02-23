f2c6148b5d2f115644ad79bbb19ace817946d7c2
Ed <silva.email@yahoo.com>
2024-02-23 06:25:48 -0500

    Add Login component and conditional rendering

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,15 @@
 import React from "react";
+import Login from "./Login";
+
+var isLoggedin = false
+
+const renderConditionally = () => {
+  if (isLoggedin === true){
+    return  <h1>Hello</h1>
+  }else {
+    return <Login />
+  }
+}
 
 function App() {
   return (
